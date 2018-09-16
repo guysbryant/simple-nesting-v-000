5fe7ebb5f631e71e9951ded88b861f44ecc6e82c
Guy Bryant <guy.s.bryant@gmail.com>
2018-09-16 23:57:34 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
+  programmer_hash[:grace_hopper]
 
 end
 
@@ -126,5 +126,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+  programmer_hash[:dennis_ritchie][:languages] << 'Assembly'
 end
